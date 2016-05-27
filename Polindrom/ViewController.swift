5e98b675409ea8cb90bd4d733413e1e4ef49aaee
Jenejkee <Zik-zhenya@mail.ru>
2016-05-28 02:57:07 +0300

    Initial Commit

---
+++ Polindrom/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Polindrom
+//
+//  Created by Евгений Сергеев on 28.05.16.
+//  Copyright © 2016 Евгений Сергеев. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
